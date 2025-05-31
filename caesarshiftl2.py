8ec0b7ec1a0a4374acf80431c4e5a952355eb751
Afif Ali Saadman <98092569+afifafifafifafifali@users.noreply.github.com>
2025-05-31 15:45:09 +0600

    Update caesarshiftl2.py

--- caesarshiftl2.py
+++ caesarshiftl2.py
@@ -12,3 +12,130 @@ def caesar_cipher(text, shift):
             shifted_char = char
         result += shifted_char
     return result
+
+def ASCII_Progress_bar(float: P)
+    floored_P = int(P)
+    display_P = (floored_P // 10) * 10
+
+    bar = "[" + "+" * (display_P // 10) + "." * (10 - (display_P // 10)) + "]"
+    print(f"{bar} {floored_P}%")
+
+#Toph : BPL Mubarak
+'''
+Outcome	Description
+N	No ball
+W	Wide ball
+D	Dead ball
+O	Out
+0-6	Runs scored
+'''
+def calculate_overs(T, outcomes):
+    results = []
+    for S in outcomes:
+        legal_balls = sum(1 for c in S if c not in ['N', 'W', 'D'])
+        overs = legal_balls // 6
+        balls = legal_balls % 6
+
+        result = []
+        if overs > 0:
+            result.append(f"{overs} OVER" if overs == 1 else f"{overs} OVERS")
+        if balls > 0 or legal_balls == 0 or (overs == 0 and balls == 0):
+            result.append(f"{balls} BALL" if balls == 1 else f"{balls} BALLS")
+
+        results.append(' '.join(result))
+    return results
+    '''
+    EG
+    T = int(input())
+outcomes = []
+for i in range(T):
+    S = input()
+    outcomes.append(S)
+
+results = calculate_overs(T, outcomes)
+
+for res in results:
+    print(res)
+'''
+
+'''
+Toph: Xtra Chocolates, Determaine how many more chocolates forhad needs to buy '''
+'''
+X, Y = map(int, input().split())
+
+remainder = Y % X
+if remainder == 0:
+    print(0)
+else:
+    print(X - remainder)
+    '''
+
+# Prochurok: The digit that is used the most in a number
+def most_used_digit(number):
+    # Convert number to string to iterate through digits
+    digits = str(number)
+    
+    # Dictionary to count occurrences of each digit
+    count = {}
+    for d in digits:
+        count[d] = count.get(d, 0) + 1
+
+    # Find the digit with the highest count
+    most_frequent = max(count, key=lambda k: (count[k], -int(k)))
+    
+    return int(most_frequent)
+
+
+'''
+Taka Breaker
+eg: 10 tk = 5+5
+'''
+'''
+N = int(input())
+
+denominations = [500, 100, 50, 10, 5, 1]
+notes = []
+
+for d in denominations:
+    count = N // d
+    N %= d
+    notes.extend([d] * count)
+
+# Sort ascending for output
+notes.sort()
+
+print(*notes)
+'''
+
+'''
+Chess Board Cell Black or white
+'''
+
+'''
+A, B = map(int, input().split())
+
+if (A + B) % 2 == 0:
+    print("Black")
+else:
+    print("White")
+'''
+
+'''
+Fibonacci nums betweeen two nums
+'''
+'''
+L, R = map(int, input().split())
+
+a, b = 0, 1
+printed_one = False
+
+while a <= R:
+    if L <= a <= R:
+        if a == 1:
+            if not printed_one:
+                print(a)
+                printed_one = True
+        else:
+            print(a)
+    a, b = b, a + b
+'''
