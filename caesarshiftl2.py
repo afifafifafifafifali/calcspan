7bc2e4eaf9fbd3755075cfc56d5ebb0f289a17b5
Afif Ali Saadman <98092569+afifafifafifafifali@users.noreply.github.com>
2025-06-01 15:38:44 +0600

    Update caesarshiftl2.py

--- caesarshiftl2.py
+++ caesarshiftl2.py
@@ -138,4 +138,56 @@ while a <= R:
         else:
             print(a)
     a, b = b, a + b
+
+    
+'''
+
+'''
+#Leetcode: Regular Expression Matching
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        # Create a DP table with (len(s)+1) x (len(p)+1)
+        dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
+        dp[0][0] = True  # Empty string matches empty pattern
+
+        # Handle patterns like a*, a*b*, a*b*c* that can match empty string
+        for j in range(2, len(p) + 1):
+            if p[j-1] == '*':
+                dp[0][j] = dp[0][j-2]
+
+        # Fill the table
+        for i in range(1, len(s) + 1):
+            for j in range(1, len(p) + 1):
+                if p[j-1] == s[i-1] or p[j-1] == '.':
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '*':
+                    dp[i][j] = dp[i][j-2]  # Treat * as 0 occurrence
+                    if p[j-2] == s[i-1] or p[j-2] == '.':
+                        dp[i][j] = dp[i][j] or dp[i-1][j]
+
+        return dp[len(s)][len(p)]
+
+'''
+
+# Int to Roman
+'''
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        base = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        sym  = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+
+        result = ""
+
+        i = 0
+        while num > 0:
+            div = num // base[i]
+            while div:
+                result += sym[i]
+                div -= 1
+
+            num %= base[i]
+            i += 1
+
+        return result
 '''
